1c1848e288f3d1e6ac21bee8b0461eee12bb2912
Ian Schoonover <iantskon@gmail.com>
2017-06-06 20:12:19 -0500

    Add owasp example

---
+++ app.js
@@ -0,0 +1,95 @@
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    owasp                 = require('owasp-password-strength-test');
+    
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Rusty is the best and cutest dog in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//============
+// ROUTES
+//============
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/secret",isLoggedIn, function(req, res){
+   res.render("secret"); 
+});
+
+// Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handling user sign up
+app.post("/register", function(req, res){
+    var result = owasp.test(req.body.password);
+    // if no results.error length then array is empty and no errors exist
+    if(!result.errors.length) {
+        User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+            if(err){
+                console.log(err);
+                return res.render('register');
+            }
+            passport.authenticate("local")(req, res, function(){
+               res.redirect("/secret");
+            });
+        });
+    } else {
+        res.render('register', {errors: result.errors});
+    }
+});
+
+// LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+app.listen(3000, function(){
+    console.log("server started.......");
+})
